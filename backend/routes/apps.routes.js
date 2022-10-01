2e3f597a2c3e8f23b65ecb3f7d6640f3f778e5c9
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-10-01 14:11:14 +0530

    apps data added

--- backend/routes/apps.routes.js
+++ backend/routes/apps.routes.js
@@ -1,13 +1,17 @@
 const { Router } = require("express");
-
+const { AppModel } = require("../models/App.model");
 const appRouter = Router();
 
-appRouter.post("/create", (req, res) => {
-  res.send("app post page");
+appRouter.post("/create", async (req, res) => {
+  const payload = req.body;
+  const newData = new AppModel(payload);
+  await newData.save();
+  res.send("Data added successfully");
 });
 
-appRouter.get("/", (req, res) => {
-  res.send("app page");
+appRouter.get("/", async (req, res) => {
+  const data = await AppModel.find({});
+  res.send(data);
 });
 
 module.exports = {
