78fee8bae6f40266d5834cdb3a6fb9fcec1ff8de
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-10-01 10:57:54 +0530

    fixing bug

--- frontend/myfitnesspal/src/App.js
+++ frontend/myfitnesspal/src/App.js
@@ -3,23 +3,25 @@ import TopNavbar from "./Components/TopNavbar";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Premium from "./pages/Premium"
+import Premium from "./pages/Premium";
 import About from "./pages/About";
-// import HomePage from "./Components/HomePage";
-// import Footer from "./Components/Footer";
+import HomePage from "./Components/HomePage";
+import Footer from "./Components/Footer";
+import { SecondaryNav } from "./Components/SecondaryNav";
 function App() {
   return (
     <div className="App">
+      <TopNavbar />
+      <SecondaryNav />
       {/* <Welcome_signup/> */}
       <Routes>
-        <Route path="/" element={ <TopNavbar/>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/premium" element={<Premium/>}/>
+        <Route path="/premium" element={<Premium />} />
       </Routes>
-      {/* <HomePage/> */}
-      {/* <Footer/> */}
-      <About/>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/myfitnesspal/src/Components/SecondaryNav.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+export const SecondaryNav = () => {
+  return (
+    <Container>
+      <SecondDiv>
+        <Link to="/about">ABOUT</Link>
+        <Link to="/food">FOOD</Link>
+        <Link to="/exercise">EXERCISE</Link>
+        <Link to="/community">COMMUNITY</Link>
+        <Link to="/blog">BLOG</Link>
+        <Link to="/premium">PREMIUM</Link>
+      </SecondDiv>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  // border: 1px solid red;
+  height: 3rem;
+  width: 100%;
+  display: flex;
+  gap: 20px;
+  background-color: rgb(0, 102, 238);
+  color: white;
+  font-weight: 100px;
+`;
+const SecondDiv = styled.div`
+  margin-left: 200px;
+  display: flex;
+  gap: 30px;
+  padding-top: 10px;
+  font-weight: 700;
+  font-size: 13px;
+  justify-content: center;
+`;
