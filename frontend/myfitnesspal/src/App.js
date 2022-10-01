c45888e08c4dc804c6afb8e2ca6ac37c3af6044c
Rohit singh <rohitsingh17031995@gmail.com>
2022-10-01 17:51:00 +0530

    added home  inside login

--- frontend/myfitnesspal/src/App.js
+++ frontend/myfitnesspal/src/App.js
@@ -5,21 +5,26 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Premium from "./pages/Premium"
 import About from "./pages/About";
-// import HomePage from "./Components/HomePage";
-// import Footer from "./Components/Footer";
+import HomePage from "./Components/HomePage";
+
+import Footer from "./Components/Footer";
+import Myhome from "./pages/Myhome";
 function App() {
   return (
     <div className="App">
       {/* <Welcome_signup/> */}
+      <TopNavbar/>
       <Routes>
-        <Route path="/" element={ <TopNavbar/>} />
+        <Route path="/" element={ <HomePage/>} />
+        <Route path="/about" element={ <About/>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/premium" element={<Premium/>}/>
+        <Route path="/myhome" element={<Myhome/>}/>
       </Routes>
       {/* <HomePage/> */}
-      {/* <Footer/> */}
-      <About/>
+      <Footer/>
+      {/* <About/> */}
     </div>
   );
 }
