484f98308d6d4ddbec5966b6edff70345542f48b
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-10-02 11:08:18 +0530

    fixing bug

--- frontend/myfitnesspal/src/App.js
+++ frontend/myfitnesspal/src/App.js
@@ -3,36 +3,26 @@ import TopNavbar from "./Components/TopNavbar";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Premium from "./pages/Premium"
+import Premium from "./pages/Premium";
 import About from "./pages/About";
 
 import BlogHomepage from "./Components/BlogHomepage";
 
-// import HomePage from "./Components/HomePage";
+import HomePage from "./Components/HomePage";
 // import Footer from "./Components/Footer";
 import { Exercise } from "./pages/Exercise";
 import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
-      {/* <Welcome_signup/> */}
+      <TopNavbar />
       <Routes>
-        <Route path="/" element={<TopNavbar />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/premium" element={<Premium />} />
-      
       </Routes>
-      {/* <HomePage/> */}
-      <Footer/>
-      {/* <About/> */}
-
-
-      <BlogHomepage/>
-
-      <Exercise />
-
-
+      <Footer />
     </div>
   );
 }
